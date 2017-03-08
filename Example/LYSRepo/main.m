bd3e4550f5dc5ebc87cec5299ad2b5ded5d3d197
刘永生 <LiuYongSheng@liuyongshengdeMacBook-Pro.local>
2017-03-08 08:59:51 +0800

    commit

---
+++ Example/LYSRepo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LYSRepo
+//
+//  Created by 3050700400@qq.com on 03/08/2017.
+//  Copyright (c) 2017 3050700400@qq.com. All rights reserved.
+//
+
+@import UIKit;
+#import "LYSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LYSAppDelegate class]));
+    }
+}
